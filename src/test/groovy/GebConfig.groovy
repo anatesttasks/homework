29c08970187be4b3622ede8a953d3d9368db9c6c
anatesttasks <51170122+anatesttasks@users.noreply.github.com>
2019-05-30 09:30:27 +0300

    Update GebConfig.groovy

--- src/test/groovy/GebConfig.groovy
+++ src/test/groovy/GebConfig.groovy
@@ -1,10 +1,3 @@
-/*
-	This is the Geb configuration file.
-	
-	See: http://www.gebish.org/manual/current/configuration.html
-*/
-
-
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 
@@ -12,12 +5,10 @@ driver = { return chromeDriver(false) }
 
 environments {
 
-	// run as “./gradlew -Dgeb.env=chrome cucumber”
 	chrome {
 		driver = { return chromeDriver(false) }
 	}
 
-	// run as “./gradlew -Dgeb.env=chromeHeadless cucumber”
 	chromeHeadless {
 		driver = { return chromeDriver(true) }
 	}
